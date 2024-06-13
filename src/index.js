23c9d7c855ac3c1d8e0f50131e557259e79e3f9f
Nguyen Dinh Chi <nguyenchi240702@gmail.com>
2024-06-13 09:41:43 +0700

    remove public/css

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(morgan("combined"));
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+// app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(5000, () => {
+  console.log("listening on port " + 5000);
+});
